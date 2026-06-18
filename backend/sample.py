310f14214b0edcbba9d443b5f92d3f64d8d32ed8
Sagar Jadhav <sagarjadhav281205@gmail.com>
2026-06-18 10:29:47 +0530

    Fix import formatting for keras in sample.py

--- backend/sample.py
+++ backend/sample.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,classification_report, confusion_matrix, f1_score
 import pickle
-import keras
+import keras 
 from tensorflow.keras import layers, Sequential 
 from tensorflow.keras.layers import Conv1D, Activation, Dropout, Dense, Flatten, MaxPooling1D
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
