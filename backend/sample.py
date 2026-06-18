d97618f90d51a3935cfcf1626528e9884f556ee4
Sagar Jadhav <sagarjadhav281205@gmail.com>
2026-06-18 10:30:14 +0530

    Update sample.py

--- backend/sample.py
+++ backend/sample.py
@@ -3,7 +3,7 @@ import os, glob, pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,classification_report, confusion_matrix, f1_score
-import pickle
+import pickle 
 import keras 
 from tensorflow.keras import layers, Sequential 
 from tensorflow.keras.layers import Conv1D, Activation, Dropout, Dense, Flatten, MaxPooling1D
